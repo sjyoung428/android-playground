d3af4412440d5c92d46aaf8cc229edb568a67b51
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-09-17 20:00:39 +0900

    feat: 버튼 상호작용

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +22,40 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setIcon(R.drawable.dino);
 
         button1 = (Button) findViewById(R.id.button1);
-
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(), "버튼을 눌렀어요", Toast.LENGTH_SHORT).show();
+                Uri webpage = Uri.parse("https://m.naver.com");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startActivity(webIntent);
+            }
+        });
+
+        button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri number = Uri.parse("tel:911");
+                Intent callIntent = new Intent(Intent.ACTION_VIEW, number);
+                startActivity(callIntent);
+            }
+        });
+
+        button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gallery = Uri.parse("content://media/internal/images/media");
+                Intent galleryIntent = new Intent(Intent.ACTION_VIEW,gallery);
+                startActivity(galleryIntent);
+            }
+        });
+
+        button4 = (Button) findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
 
