572ec79e9be82c44a25e6b93846bf5deddb84fe4
codeyoung <77968955+sjyoung123@users.noreply.github.com>
2022-09-28 00:44:18 +0900

    feat: 동물 고르기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,18 +1,24 @@
 package com.example.myapplication;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1;
-    Button button2;
-    Button button3;
-    Button button4;
+    TextView choiceText, KindOfAnimal;
+    CheckBox checkBox;
+    RadioGroup radioGroup;
+    RadioButton dog,cat,rabbit;
+    Button button;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,44 +26,50 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.drawable.dino);
+        setTitle("애완동물 사진 보기");
 
-        button1 = (Button) findViewById(R.id.button1);
-        button1.setOnClickListener(new View.OnClickListener() {
+        choiceText = (TextView) findViewById(R.id.choiceText);
+        KindOfAnimal = (TextView) findViewById(R.id.kindOfAnimal);
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        dog = (RadioButton) findViewById(R.id.dog);
+        cat = (RadioButton) findViewById(R.id.cat);
+        rabbit = (RadioButton) findViewById(R.id.rabbit);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+
+        checkBox = (CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Uri webpage = Uri.parse("https://m.naver.com");
-                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
-                startActivity(webIntent);
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(checkBox.isChecked()){
+                    KindOfAnimal.setVisibility(View.VISIBLE);
+                    radioGroup.setVisibility(View.VISIBLE);
+                    button.setVisibility(View.VISIBLE);
+                    imageView.setVisibility(View.VISIBLE);
+                }else{
+                    KindOfAnimal.setVisibility(View.INVISIBLE);
+                    radioGroup.setVisibility(View.INVISIBLE);
+                    button.setVisibility(View.INVISIBLE);
+                    imageView.setVisibility(View.INVISIBLE);
+                }
             }
         });
 
-        button2 = (Button) findViewById(R.id.button2);
-        button2.setOnClickListener(new View.OnClickListener() {
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri number = Uri.parse("tel:911");
-                Intent callIntent = new Intent(Intent.ACTION_VIEW, number);
-                startActivity(callIntent);
+                switch (radioGroup.getCheckedRadioButtonId()){
+                    case R.id.dog:
+                        imageView.setImageResource(R.drawable.dog);
+                    case R.id.cat:
+                        imageView.setImageResource(R.drawable.cat);
+                    case R.id.rabbit:
+                        imageView.setImageResource(R.drawable.rabbit);
+                }
             }
         });
 
-        button3 = (Button) findViewById(R.id.button3);
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri gallery = Uri.parse("content://media/internal/images/media");
-                Intent galleryIntent = new Intent(Intent.ACTION_VIEW,gallery);
-                startActivity(galleryIntent);
-            }
-        });
-
-        button4 = (Button) findViewById(R.id.button4);
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
 
     }
 }
